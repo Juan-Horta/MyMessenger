16c10f5001503dc4cf1e3ddc976a8ff99143c1fc
Juan-Horta <juanhorta1516@gmail.com>
2022-09-08 15:27:29 -0500

    Final Chat

--- app/src/main/java/co/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
+++ app/src/main/java/co/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
@@ -4,20 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "message";
+    public Button boton;
+    public EditText text;
+    public static final String EXTRA_MESSAGE = "mensaje";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boton = findViewById(R.id.send);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
-
         Intent intent = getIntent();
-        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
-        TextView messageView = (TextView) findViewById(R.id.message);
-        messageView.setText(messageText);
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView viewMessage = findViewById(R.id.textView);
+        viewMessage.setText(message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+        startActivity(intent);
     }
 }
